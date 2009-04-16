2c89b21f8263923a81660a1292e0ad2d44ef4128
Erik Bryn <erik.bryn@gmail.com>
2009-04-17 02:29:55 +0800

    Conditionally defining #blank? on String/NilClass
    Signed-off-by: Chris Eppstein <chris@eppsteins.net>

--- lib/compass/core_ext.rb
+++ lib/compass/core_ext.rb
@@ -1,12 +1,14 @@
 class String
-
-  # see if string has any content
-  def blank?; self.length.zero?; end
-  
+  unless method_defined?(:blank?)
+    # see if string has any content
+    def blank?; self.length.zero?; end
+  end
 end
 
 class NilClass
-  def blank?
-    true
+  unless method_defined?(:blank?)
+    def blank?
+      true
+    end
   end
 end
