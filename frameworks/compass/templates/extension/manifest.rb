8fa32acf79a86fa35be753e5bc894f4ec915f4d2
Chris Eppstein <chris@eppsteins.net>
2009-08-30 02:22:52 -0700

    The extension pattern works better when using the pattern name instead of the project name for the target sass file.

--- frameworks/compass/templates/extension/manifest.rb
+++ frameworks/compass/templates/extension/manifest.rb
@@ -1,4 +1,4 @@
-file 'stylesheets/main.sass', :to => "stylesheets/_#{File.basename(options[:project_name])}.sass"
+file 'stylesheets/main.sass', :to => "stylesheets/_#{File.basename(options[:pattern_name])}.sass"
 file 'templates/project/manifest.rb'
 file 'templates/project/screen.sass'
 
