b8d380d68c519124d532be85bd093db6e366e60e
Chris Eppstein <chris@eppsteins.net>
2009-10-04 22:17:56 -0700

    Version subcommand.

--- lib/compass/commands.rb
+++ lib/compass/commands.rb
@@ -5,6 +5,7 @@ require 'compass/commands/registry'
 
 %w(base generate_grid_background help list_frameworks project_base
    update_project watch_project create_project installer_command
-   print_version stamp_pattern validate_project	write_configuration).each do |lib|
+   print_version stamp_pattern validate_project
+   write_configuration).each do |lib|
   require "compass/commands/#{lib}"
 end
