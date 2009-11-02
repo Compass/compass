1981e5f2ca24a33fa338b7a6eff4689d617aeadf
Chris Eppstein <chris@eppsteins.net>
2009-11-01 17:40:50 -0800

    Help for each template that ships with compass.

--- lib/compass/frameworks/blueprint/templates/link_icons/manifest.rb
+++ lib/compass/frameworks/blueprint/templates/link_icons/manifest.rb
@@ -1,3 +1,5 @@
+description "Icons for common types of links"
+
 stylesheet "link_icons.sass", :media => 'screen, projection'
 
 image 'link_icons/doc.png'
@@ -8,3 +10,14 @@ image 'link_icons/im.png'
 image 'link_icons/pdf.png'
 image 'link_icons/visited.png'
 image 'link_icons/xls.png'
+
+help %Q{
+To install the link_icons plugin:
+  compass init --using blueprint/link_icons
+
+The link_icons.sass file is just a recommendation to show you how to use the link mixins.  
+}
+
+welcome_message %Q{
+The link_icons.sass file is just a recommendation to show you how to use the link mixins.  
+}
