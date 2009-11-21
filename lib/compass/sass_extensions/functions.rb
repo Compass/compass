9d8253fa051d39215315f138f902716829c9b915
Chris Eppstein <chris@eppsteins.net>
2009-11-21 09:26:54 -0800

    Added a color-stop function to simplify the creation of gradients with multiple stop points.

--- lib/compass/sass_extensions/functions.rb
+++ lib/compass/sass_extensions/functions.rb
@@ -1,7 +1,7 @@
 module Compass::SassExtensions::Functions
 end
 
-%w(selectors enumerate urls display inline_image).each do |func|
+%w(selectors enumerate urls display inline_image color_stop).each do |func|
   require "compass/sass_extensions/functions/#{func}"
 end
 
@@ -11,6 +11,7 @@ module Sass::Script::Functions
   include Compass::SassExtensions::Functions::Urls
   include Compass::SassExtensions::Functions::Display
   include Compass::SassExtensions::Functions::InlineImage
+  include Compass::SassExtensions::Functions::ColorStop
 end
 
 # Wierd that this has to be re-included to pick up sub-modules. Ruby bug?
