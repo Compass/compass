7da41d027a348888180d8ef33672fd0cde4d2c9a
Chris Eppstein <chris@eppsteins.net>
2009-11-25 22:55:42 -0800

    [Compass Core] +font-face mixin uses stylesheet_url to form urls relative to the css directory.

--- lib/compass/sass_extensions/functions/font_files.rb
+++ lib/compass/sass_extensions/functions/font_files.rb
@@ -3,7 +3,7 @@ module Compass::SassExtensions::Functions::FontFiles
     raise Sass::SyntaxError, "An even number of arguments must be passed to font_files()" unless args.size % 2 == 0
     files = []
     while args.size > 0
-      files << "url('#{args.shift}') format('#{args.shift}')"
+      files << "#{stylesheet_url(args.shift)} format('#{args.shift}')"
     end
     Sass::Script::String.new(files.join(", "))
   end
