02e2d7a4a98bae2f8de01b91f46c19d809fb3700
Chris Eppstein <chris@eppsteins.net>
2010-01-02 11:11:45 -0800

    Upgrade to FSSM v0.1.2.

--- lib/vendor/fssm/support.rb
+++ lib/vendor/fssm/support.rb
@@ -39,10 +39,11 @@ module FSSM::Support
     def rb_inotify?
       begin
         require 'rubygems'
+        gem 'rb-inotify', '>= 0.3.0'
         require 'rb-inotify'
         true
       rescue LoadError, Gem::LoadError
-        STDERR.puts("Warning: Unable to load rb-inotify. Inotify will be unavailable.")
+        STDERR.puts("Warning: Unable to load rb-inotify >= 0.3.0. Inotify will be unavailable.")
         false
       end
     end
