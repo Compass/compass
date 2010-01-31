545525deb7fcfdb112eacd1765ec5fdcb90e39e9
Chris Eppstein <chris@eppsteins.net>
2010-01-31 09:43:19 -0800

    Tell compass we're under /docs

--- doc-src/.compass/config.rb
+++ doc-src/.compass/config.rb
@@ -2,14 +2,14 @@
 # require 'susy'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/docs"
 project_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 css_dir = "../docs/stylesheets"
 sass_dir = "content/stylesheets"
 images_dir = "assets/images"
-http_images_dir = "images"
 javascripts_dir = "content/javascripts"
 http_javascripts_dir = "javascripts"
 http_stylesheets_dir = "stylesheets"
+http_images_dir = "images"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
