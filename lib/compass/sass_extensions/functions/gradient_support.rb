ad4a486ea1ea3123e2182c090c17674d38de8d90
Chris Eppstein <chris@eppsteins.net>
2010-02-21 22:44:38 -0800

    Simpler gradient output by discarding from() and to()

--- lib/compass/sass_extensions/functions/gradient_support.rb
+++ lib/compass/sass_extensions/functions/gradient_support.rb
@@ -25,8 +25,11 @@ module Compass::SassExtensions::Functions::GradientSupport
       s = "#{color}"
       if stop
         s << " "
-        s << stop.times(Sass::Script::Number.new(100)).to_s
-        s << "%"
+        if stop.unitless?
+          s << stop.times(Sass::Script::Number.new(100, ["%"])).to_s
+        else
+          s << stop.to_s
+        end
       end
       s
     end
@@ -41,6 +44,7 @@ module Compass::SassExtensions::Functions::GradientSupport
         when "bottom" then "top"
         when "left" then "right"
         when "right" then "left"
+        when "center" then "center"
         else
           raise Sass::SyntaxError, "Cannot determine the opposite of #{pos}"
         end
@@ -50,49 +54,53 @@ module Compass::SassExtensions::Functions::GradientSupport
 
     # returns color-stop() calls for use in webkit.
     def grad_color_stops(color_list)
-      positions = color_list.values.map{|c| [c.stop && c.stop.value, c.color]}
-      # fill in the blank positions
-      positions[0][0] = 0 unless positions.first.first
-      positions[positions.size - 1][0] = 1 unless positions.last.first
-      for i in 0...positions.size
-        if positions[i].first.nil?
-          num = 2.0
-          for j in (i+1)...positions.size
-            if positions[j].first
-              positions[i][0] = positions[i-1].first + (positions[j].first - positions[i-1].first) / num
-              break
-            else
-              num += 1
-            end
+      normalize_stops!(color_list)
+      max = color_list.values.last.stop
+      color_stops = color_list.values.map do |pos|
+        # have to convert absolute units to percentages for use in color stop functions.
+        stop = pos.stop
+        stop = stop.div(max).times(Sass::Script::Number.new(100,["%"])) if stop.numerator_units == max.numerator_units
+        "color-stop(#{stop}, #{pos.color})"
+      end
+      Sass::Script::String.new(color_stops.join(", "))
+    end
+    
+    # returns the end position of the gradient from the color stop
+    def grad_end_position(color_list, radial = Sass::Script::Bool.new(false))
+      default = Sass::Script::Number.new(100)
+      grad_position(color_list, Sass::Script::Number.new(color_list.values.size), default, radial)
+    end
+
+    def grad_position(color_list, index, default, radial = Sass::Script::Bool.new(false))
+      stop = color_list.values[index.value - 1].stop
+      if stop && radial.to_bool
+        orig_stop = stop
+        if stop.unitless?
+          if stop.value <= 1
+            # A unitless number is assumed to be a percentage when it's between 0 and 1
+            stop = stop.times(Sass::Script::Number.new(100, ["%"]))
+          else
+            # Otherwise, a unitless number is assumed to be in pixels
+            stop = stop.times(Sass::Script::Number.new(1, ["px"]))
           end
         end
-      end
-      positions = positions[1..-1] if positions.first.first == 0.0
-      positions = positions[0..-2] if positions.last.first == 1.0
-      if positions.empty?
-        # We return false here since sass has no nil.
-        Sass::Script::Bool.new(false)
+        if stop.numerator_units == ["%"] && color_list.values.last.stop && color_list.values.last.stop.numerator_units == ["px"]
+          stop = stop.times(color_list.values.last.stop).div(Sass::Script::Number.new(100, ["%"]))
+        end
+        Compass::Logger.new.record(:warning, "Webkit only supports pixels for the start and end stops for radial gradients. Got: #{orig_stop}") if stop.numerator_units != ["px"]
+        stop.div(Sass::Script::Number.new(1, stop.numerator_units, stop.denominator_units))
+      elsif stop
+        stop
       else
-        color_stops = positions.map {|pos| "color-stop(#{Sass::Script::Number.new((pos.first*10000).round/100.0).to_s}%, #{pos.last})"}
-        Sass::Script::String.new(color_stops.join(", "))
+        default
       end
-    end
-
-    # the first color from a list of color stops
-    def grad_start_color(color_list)
-      color_list.values.first.color
-    end
-
-    # the last color from a list of color stops
-    def grad_end_color(color_list)
-      color_list.values.last.color
     end
 
     # the given a position, return a point in percents
     def grad_point(position)
       position = position.value
       position = if position[" "]
-        if position =~ /(top|bottom) (left|right)/
+        if position =~ /(top|bottom|center) (left|right|center)/
           "#{$2} #{$1}"
         else
           position
@@ -103,9 +111,16 @@ module Compass::SassExtensions::Functions::GradientSupport
           "left #{position}"
         when /left|right/
           "#{position} top"
+        else
+          position
         end
       end
-      Sass::Script::String.new position.gsub(/top/, "0%").gsub(/bottom/, "100%").gsub(/left/,"0%").gsub(/right/,"100%")
+      Sass::Script::String.new(position.
+        gsub(/top/, "0%").
+        gsub(/bottom/, "100%").
+        gsub(/left/,"0%").
+        gsub(/right/,"100%").
+        gsub(/center/, "50%"))
     end
 
     def color_stops(*args)
@@ -125,13 +140,47 @@ module Compass::SassExtensions::Functions::GradientSupport
             end
             stop = Sass::Script::Number.new(number)
           elsif !stop.nil?
-            raise Sass::SyntaxError, "A color stop location must be between 0 and 1 or 0% and 100%. Got: #{stop}"          
+            number = Sass::Script::Parser.parse(stop, 0, 0)
+            unless number.is_a?(Sass::Script::Number)
+              raise Sass::SyntaxError, "A color stop location must be a number. Got: #{stop}"
+            end
+            stop = number
           end
           ColorStop.new(color, stop)
         else
           raise Sass::SyntaxError, "Not a valid color stop: #{arg}"          
         end
       end)
+    end
+    private
+    def normalize_stops!(color_list)
+      positions = color_list.values
+      # fill in the start and end positions, if unspecified
+      positions.first.stop = Sass::Script::Number.new(0) unless positions.first.stop
+      positions.last.stop = Sass::Script::Number.new(100, ["%"]) unless positions.last.stop
+      # fill in empty values
+      for i in 0...positions.size
+        if positions[i].stop.nil?
+          num = 2.0
+          for j in (i+1)...positions.size
+            if positions[j].stop
+              positions[i].stop = positions[i-1].stop.plus((positions[j].stop.minus(positions[i-1].stop)).div(Sass::Script::Number.new(num)))
+              break
+            else
+              num += 1
+            end
+          end
+        end
+      end
+      # normalize unitless numbers
+      positions.each do |pos|
+        if pos.stop.unitless? && pos.stop.value <= 1
+          pos.stop = pos.stop.times(Sass::Script::Number.new(100, ["%"]))
+        elsif pos.stop.unitless?
+          pos.stop = pos.stop.times(Sass::Script::Number.new(1, ["px"]))
+        end
+      end
+      nil
     end
   end
 end
