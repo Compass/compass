c3f887ff51fa8154d89e95c629c2257e7bd0cdf0
Chris Eppstein <chris@eppsteins.net>
2010-02-23 13:12:22 -0800

    [Rails] Fix rails boot sequence in Rails 2.3.x environments.

--- lib/compass/app_integration/rails/runtime.rb
+++ lib/compass/app_integration/rails/runtime.rb
@@ -1,12 +1,18 @@
 unless defined?(Compass::RAILS_LOADED)
   Compass::RAILS_LOADED = true
-  
-  if ActionPack::VERSION::MAJOR >= 3
-    # figure something out so image_path works with rails integration
-  else
-    %w(action_controller sass_plugin urls).each do |lib|
-      require "compass/app_integration/rails/actionpack2/#{lib}"
+  begin
+    require 'action_pack/version'
+    if ActionPack::VERSION::MAJOR >= 3
+      # TODO figure something out so image_path works with rails integration
+    else
+      %w(action_controller sass_plugin urls).each do |lib|
+        require "compass/app_integration/rails/actionpack2/#{lib}"
+      end
     end
+  rescue LoadError => e
+    $stderr.puts "Compass could not access the rails environment."
+  rescue NameError => e
+    $stderr.puts "Compass could not access the rails environment."
   end
   
   # Wierd that this has to be re-included to pick up sub-modules. Ruby bug?
