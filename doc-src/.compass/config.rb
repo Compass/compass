128a5519600a81e0d5a231e2af6ed10eeb8303ab
Chris Eppstein <chris@eppsteins.net>
2010-03-28 23:53:57 -0700

    Font face example.

--- doc-src/.compass/config.rb
+++ doc-src/.compass/config.rb
@@ -7,9 +7,11 @@ project_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 css_dir = "../docs/stylesheets"
 sass_dir = "content/stylesheets"
 images_dir = "assets/images"
-javascripts_dir = "content/javascripts"
+javascripts_dir = "assets/javascripts"
+fonts_dir = "assets/fonts"
 http_javascripts_dir = "javascripts"
 http_stylesheets_dir = "stylesheets"
 http_images_dir = "images"
+http_fonts_dir = "fonts"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
