cebd6854688537383375dfd1269d0c6ac8df1ad5
Chris Eppstein <chris@eppsteins.net>
2010-05-16 10:16:13 -0700

    Fix a bug in the version code that only manifests when there's no patch release version.

--- lib/compass/version.rb
+++ lib/compass/version.rb
@@ -24,7 +24,8 @@ module Compass
       require 'yaml'
       @version = YAML::load(File.read(scope('VERSION.yml')))
       @version[:teeny] = @version[:patch]
-      @version[:string] = "#{@version[:major]}.#{@version[:minor]}.#{@version[:patch]}"
+      @version[:string] = "#{@version[:major]}.#{@version[:minor]}"
+      @version[:string] << ".#{@version[:patch]}" if @version[:patch]
       @version[:string] << ".#{@version[:state]}" if @version[:state]
       @version[:string] << ".#{@version[:build]}" if @version[:build]
       if !ENV['OFFICIAL'] && r = revision
