3e7cd28635b1a3624cf2fbd685a22bad625488e9
Nico Hagenburger <nico@hagenburger.net>
2010-10-17 15:27:59 +0200

    added sprite position calculating

--- lib/compass/sass_extensions/functions/sprites.rb
+++ lib/compass/sass_extensions/functions/sprites.rb
@@ -1,4 +1,21 @@
 module Compass::SassExtensions::Functions::Sprites
+  def sprite_image(uri)
+    uri = uri.value
+    path, name = Compass::Sprites.path_and_name(uri)
+    y = 0
+    last_spacing = 0
+    images = Compass::Sprites.sprites(name)
+    images.each do |image|
+      current_spacing = number_from_var("#{name}-#{image[:name]}-spacing")
+      if y > 0
+        y += current_spacing > last_spacing ? current_spacing : last_spacing
+      end
+      image[:y] = y
+      y += image[:height]
+      last_spacing = current_spacing
+    end
+    image_url(Sass::Script::String.new("#{path}.png"))
+  end
 
   def sprite_position(file)
     name = File.dirname(file.value)
@@ -7,5 +24,14 @@ module Compass::SassExtensions::Functions::Sprites
     y = "-#{y}px" unless y == 0
     Sass::Script::String.new("0 #{y}")
   end
-
+  
+private
+  
+  def number_from_var(var_name)
+    if var = environment.var(var_name)
+      var.value
+    else
+      0
+    end
+  end
 end
