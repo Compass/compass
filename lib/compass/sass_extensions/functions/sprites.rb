df05620508b6034756aceaf01453af005a28fb57
Nico Hagenburger <nico@hagenburger.net>
2010-10-17 19:38:32 +0200

    added positioning of images

--- lib/compass/sass_extensions/functions/sprites.rb
+++ lib/compass/sass_extensions/functions/sprites.rb
@@ -1,11 +1,12 @@
 require 'chunky_png'
 
 module Compass::SassExtensions::Functions::Sprites
+  SASS_NULL = Sass::Script::Number::new(0)
+  
   def sprite_image(uri)
     uri = uri.value
     path, name = Compass::Sprites.path_and_name(uri)
     last_spacing = 0
-    default_spacing = number_from_var("#{name}-spacing")
     width = 0
     height = 0
     images = Compass::Sprites.sprites(name)
@@ -14,7 +15,7 @@ module Compass::SassExtensions::Functions::Sprites
     images.each do |image|
       current_spacing = number_from_var("#{name}-#{image[:name]}-spacing")
       if height > 0
-        height += [current_spacing, last_spacing, default_spacing].max
+        height += [current_spacing, last_spacing].max
       end
       image[:y] = height
       height += image[:height]
@@ -26,21 +27,32 @@ module Compass::SassExtensions::Functions::Sprites
     output_png = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
     images.each do |image|
       input_png  = ChunkyPNG::Image.from_file(image[:file])
-      x = 0
-      y = image[:y]
-      output_png.replace input_png, x, y
+      position = environment.var("#{name}-#{image[:name]}-position")
+      if position.unit_str == "%"
+        image[:x] = (width - image[:width]) * (position.value / 100)
+      else
+        image[:x] = position.value
+      end
+      output_png.replace input_png, image[:x], image[:y]
     end
     output_png.save File.join(File.join(Compass.configuration.images_path, "#{path}.png"))
     
     image_url(Sass::Script::String.new("#{path}.png"))
   end
 
-  def sprite_position(file)
+  def sprite_position(file, x_shift = SASS_NULL, y_shift = SASS_NULL)
     name = File.dirname(file.value)
-    sprite = File.basename(file.value, '.png')
-    y = Compass::Sprites.sprites(name).detect{ |sprite_info| sprite_info[:name] == sprite }[:y]
-    y = "-#{y}px" unless y == 0
-    Sass::Script::String.new("0 #{y}")
+    image_name = File.basename(file.value, '.png')
+    image = Compass::Sprites.sprites(name).detect{ |image| image[:name] == image_name }
+    if x_shift.unit_str == "%"
+      x = x_shift.to_s
+    else
+      x = x_shift.value - image[:x]
+      x = "#{x}px" unless x == 0
+    end
+    y = y_shift.value - image[:y]
+    y = "#{y}px" unless y == 0
+    Sass::Script::String.new("#{x} #{y}")
   end
   
 private
