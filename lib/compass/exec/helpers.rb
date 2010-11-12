183a67b0c027b17b0d5506b4db7d4f6b5c21e0a4
Chris Eppstein <chris@eppsteins.net>
2010-11-11 21:01:42 -0800

    Revert "Use the new Sass gem now."
    This reverts commit 822f35148be74fee4f80ed4881314c61c4543c85.
    
    Conflicts:
    
    	Gemfile

--- lib/compass/exec/helpers.rb
+++ lib/compass/exec/helpers.rb
@@ -7,7 +7,7 @@ module Compass::Exec
       else
         unless arguments.include?("-h") || arguments.include?("--help")
           Compass::Logger.new.red do
-            Sass::Util.sass_warn "WARNING: This interface is deprecated. Please use the new subcommand interface.\nSee `compass help` for more information.\n"
+            Haml::Util.haml_warn "WARNING: This interface is deprecated. Please use the new subcommand interface.\nSee `compass help` for more information.\n"
           end
         end
         SwitchUI
