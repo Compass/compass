d4a048492252a8a27d90de33a690aedbcafe2c14
Chris Eppstein <chris@eppsteins.net>
2010-11-15 03:47:57 -0800

    Return false if no false value is provided.

--- lib/compass/sass_extensions/functions/if.rb
+++ lib/compass/sass_extensions/functions/if.rb
@@ -1,9 +1,9 @@
 module Compass::SassExtensions::Functions::If
-  def if(truth, if_true, if_false)
+  def if(truth, if_true, if_false = nil)
     if truth.to_bool
       if_true
     else
-      if_false
+      if_false || Sass::Script::Bool.new(false)
     end
   end
 end
