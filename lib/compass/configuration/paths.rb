dabd4f6f9d5acd8526107a75a0cdc06d517a09e9
Chris Eppstein <chris@eppsteins.net>
2010-11-15 18:01:04 -0800

    Rails gives us a pathname, pathnames don't like =~

--- lib/compass/configuration/paths.rb
+++ lib/compass/configuration/paths.rb
@@ -8,7 +8,7 @@ module Compass::Configuration::Paths
       class_eval %Q{
         def #{attr}                                # def css_dir
           path = #{attr}_with_trailing_separator   #   path = css_dir_with_trailing_separator
-          if path =~ TRAILING_SEPARATOR            #   if path =~ TRAILING_SEPARATOR
+          if path.to_s =~ TRAILING_SEPARATOR       #   if path =~ TRAILING_SEPARATOR
             path = path[0..-($1.length+1)]         #     path = path[0..-($1.length+1)]
           end                                      #   end
           path                                     #   path
