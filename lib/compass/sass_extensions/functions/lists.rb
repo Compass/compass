f06d928c4b917b574bd9fe152be9a5cfbce4d1b7
Chris Eppstein <chris@eppsteins.net>
2010-11-21 15:12:52 -0800

    Support for sass 3.1 alphas

--- lib/compass/sass_extensions/functions/lists.rb
+++ lib/compass/sass_extensions/functions/lists.rb
@@ -2,6 +2,8 @@ module Compass::SassExtensions::Functions::Lists
   def first_value_of(list)
     if list.is_a?(Sass::Script::String)
       Sass::Script::String.new(list.value.split(/\s+/).first)
+    elsif defined?(Sass::Script::List) && list.is_a?(Sass::Script::List)
+      list.value.first
     else
       list
     end
