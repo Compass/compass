5fefd805d090b005d912e1e226dfbf77b8dccf67
Robert Dougan <rdougan@me.com>
2010-12-03 17:30:22 -0800

    Adding second-value-of, third-value-of and fourth-value-of functions into the SASS Extensions. first-value-of already exists. All functions default to first-value-of if 2,3,4 value doesn't exist.

--- lib/compass/sass_extensions/functions/lists.rb
+++ lib/compass/sass_extensions/functions/lists.rb
@@ -1,11 +1,28 @@
 module Compass::SassExtensions::Functions::Lists
-  def first_value_of(list)
+  def value_of(int, list)
     if list.is_a?(Sass::Script::String)
-      Sass::Script::String.new(list.value.split(/\s+/).first)
+      values = list.value.split(/\s+/)
+      Sass::Script::String.new(values[int] || values.first)
     elsif defined?(Sass::Script::List) && list.is_a?(Sass::Script::List)
-      list.value.first
+      list.value[int] || list.value.first
     else
       list
     end
+  end
+  
+  def first_value_of(list)
+    self.value_of(0, list)
+  end
+  
+  def second_value_of(list)
+    self.value_of(1, list)
+  end
+  
+  def third_value_of(list)
+    self.value_of(2, list)
+  end
+  
+  def fourth_value_of(list)
+    self.value_of(3, list)
   end
 end
