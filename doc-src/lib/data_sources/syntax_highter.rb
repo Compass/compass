2da5502040cb50577574db21cb851f95c78ffa99
Brandon Mathis <brandon@imathis.com>
2010-12-15 18:15:19 -0600

    improved syntax highlighting

--- doc-src/lib/data_sources/syntax_highter.rb
+++ doc-src/lib/data_sources/syntax_highter.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'coderay'
 class SyntaxHighlighterFilter < Nanoc3::Filter
   identifier :highlight
-  
+
   def highlight(code, type)
     hl_map = Hash.new(:coderay)
     hl_map[:sass] = :pygmentize
@@ -17,7 +17,7 @@ class SyntaxHighlighterFilter < Nanoc3::Filter
       return io.read
     end
   end
-  
+
   def coderay(code, type)
     # :line_numbers => :table,
     type = :css if type == :scss
@@ -26,12 +26,12 @@ class SyntaxHighlighterFilter < Nanoc3::Filter
 
   def run(content, params={})
     doc = Nokogiri::HTML.fragment(content)
-    [:css, :sass, :scss].each do |format|
+    [:css, :sass, :scss, :html, :haml].each do |format|
       doc.css("pre.source-code.#{format}, code.#{format}").each do |el|
         el.set_attribute("class", "brush: #{format} "+el.attribute("class").value)
       end
     end
     doc.to_s
   end
-  
+
 end
