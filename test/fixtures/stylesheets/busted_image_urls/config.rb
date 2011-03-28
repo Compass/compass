21928907e4ae8f739cc9baf006d580ce7d2aaf07
Nathaniel Bibler <git@nathanielbibler.com>
2011-03-27 22:35:49 -0400

    Extend cache busting to allow for path manipulation

---
+++ test/fixtures/stylesheets/busted_image_urls/config.rb
@@ -0,0 +1,20 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+css_dir = "tmp"
+sass_dir = "sass"
+images_dir = "images"
+output_style = :compact
+# To enable relative image paths using the images_url() function:
+# http_images_path = :relative
+http_images_path = "/images"
+line_comments = false
+
+asset_cache_buster do |path, file|
+  pathname = Pathname.new(path)
+  "%s/%s-BUSTED%s" % [pathname.dirname, pathname.basename(pathname.extname), pathname.extname]
+end
+
+
+asset_host do |path|
+  "http://assets%d.example.com" % (path.size % 4)
+end
