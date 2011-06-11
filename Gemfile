ecd1efdea47a59f743580e0baa8072d5de8579bc
Chris Eppstein <chris@eppsteins.net>
2011-06-11 11:33:01 -0700

    Fix for a compass validator bug due to some internal compass changes.

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gemspec
 gem "cucumber", "~> 0.9.2"
 gem "rspec", "~>2.0.0"
 gem "rails", "~>3.0.0.rc"
-gem "compass-validator", "3.0.0"
+gem "compass-validator", "3.0.1"
 gem "css_parser", "~> 1.0.1"
 gem "sass", "~>3.1"
 gem "haml", "~> 3.1"
--- lib/compass/validator.rb
+++ lib/compass/validator.rb
@@ -1,9 +1,16 @@
 begin
   require 'rubygems'
   require 'compass-validator'
-rescue LoadError
-  raise Compass::MissingDependency, %Q{The Compass CSS Validator could not be loaded. Please install it:
-
+rescue LoadError => e
+  if e.message =~ /core_ext/
+    raise Compass::MissingDependency, <<-ERRORMSG
+The Compass CSS Validator is out of date. Please upgrade it:
+sudo gem install compass-validator --version ">= 3.0.1"
+ERRORMSG
+  else
+    raise Compass::MissingDependency, <<-ERRORMSG
+The Compass CSS Validator could not be loaded. Please install it:
 sudo gem install compass-validator
-}
+ERRORMSG
+  end
 end
