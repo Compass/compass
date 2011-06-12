7454e522a5dd41a2cc107daf6eaa4171d01c0984
Scott Davis <jetviper21@gmail.com>
2011-06-12 00:52:36 -0400

    engine abstract

--- lib/compass/sass_extensions/sprites/engines.rb
+++ lib/compass/sass_extensions/sprites/engines.rb
@@ -1,1 +1,24 @@
+module Compass
+  module SassExtensions
+    module Sprites
+      class Engine
+        attr_accessor :width, :height, :images
+        def initialize(width, height, images)
+          @width, @height, @images = width, height, images
+        end
+        
+        def construct_sprite
+          raise ::Compass::Error, "You must impliment construct_sprite"
+        end
+        
+        def save(filename)
+          raise ::Compass::Error, "You must impliment save(filename)"
+        end
+        
+      end
+    end
+  end
+end
+
+
 require 'compass/sass_extensions/sprites/engines/chunky_png_engine'
