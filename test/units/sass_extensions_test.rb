acfd1e43a2d7332fde7e960adb890ca86f573ed9
Scott Davis <jetviper21@gmail.com>
2011-08-02 18:49:06 -0400

    fixed trig functions closes #498

--- test/units/sass_extensions_test.rb
+++ test/units/sass_extensions_test.rb
@@ -65,18 +65,21 @@ class SassExtensionsTest < Test::Unit::TestCase
   end
 
   def test_trig_functions
+    assert_equal "0.841", evaluate("sin(1)")
     assert_equal "0.841px", evaluate("sin(1px)")
     assert_equal "0.0", evaluate("sin(pi())")
     assert_equal "1",   evaluate("sin(pi() / 2)")
-    assert_equal "1",   evaluate("sin(180deg)")
+    assert_equal "0.0",   evaluate("sin(180deg)")
     assert_equal "-1",  evaluate("sin(3* pi() / 2)")
     assert_equal "-1", evaluate("cos(pi())")
-    assert_equal "-1", evaluate("cos(360deg)")
+    assert_equal "1", evaluate("cos(360deg)")
+    assert_equal "-0.176", evaluate("sin(270)")
     assert_equal "1", evaluate("cos(2*pi())")
     assert_equal "0.0",   evaluate("cos(pi() / 2)")
     assert_equal "0.0",  evaluate("cos(3* pi() / 2)")
     assert_equal "0.0",  evaluate("tan(pi())")
     assert_equal "0.0", evaluate("tan(360deg)")
+    assert_equal "0.959", evaluate("sin(360)")
     assert evaluate("tan(pi()/2 - 0.0001)").to_f > 1000, evaluate("tan(pi()/2 - 0.0001)")
     assert evaluate("tan(pi()/2 + 0.0001)").to_f < -1000, evaluate("tan(pi()/2 - 0.0001)")
   end
