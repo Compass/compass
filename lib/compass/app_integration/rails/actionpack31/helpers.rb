d0bbb66e73dd222824c23c2cc55735af4031fddc
Chris Eppstein <chris@eppsteins.net>
2011-08-28 12:54:52 -0700

    Add a generated-image-url helper for rails31 specifically

--- lib/compass/app_integration/rails/actionpack31/helpers.rb
+++ lib/compass/app_integration/rails/actionpack31/helpers.rb
@@ -1,5 +1,5 @@
 module Sass::Script::Functions
-  def generated_image_url(path)
-    asset_url(path, "image")
+  def generated_image_url(path, only_path = nil)
+    asset_url(path, Sass::Script::String.new("image"))
   end
 end
