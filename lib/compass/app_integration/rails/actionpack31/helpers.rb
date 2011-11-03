eb10566e02e4539ff0bbb976db34a1cbf0028699
Scott Davis <jetviper21@gmail.com>
2011-11-03 15:45:32 -0400

    fixed and issue with Sprokets trying to the the supreme ruler of the file paths!

--- lib/compass/app_integration/rails/actionpack31/helpers.rb
+++ lib/compass/app_integration/rails/actionpack31/helpers.rb
@@ -9,10 +9,12 @@ module Compass::RailsImageFunctionPatch
   private
   
   def image_path_for_size(image_file)
-    if file = ::Rails.application.assets.find_asset(image_file)
+    begin
+      file = ::Rails.application.assets.find_asset(image_file)
       return file
+    rescue ::Sprockets::FileOutsidePaths
+      return super(image_file)
     end
-    super(image_file)
   end
 end
 
