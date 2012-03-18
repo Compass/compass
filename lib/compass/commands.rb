dd629b1b23900806292a56ed95f72d1d96e19798
Chris Eppstein <chris@eppsteins.net>
2012-03-17 17:04:35 -0700

    Extension manager command (a trivial front-end for the gem command)

--- lib/compass/commands.rb
+++ lib/compass/commands.rb
@@ -4,8 +4,9 @@ end
 require 'compass/commands/registry'
 
 %w(base generate_grid_background default help list_frameworks project_base
-   update_project watch_project create_project clean_project imports installer_command
-   print_version project_stats stamp_pattern sprite validate_project
-   write_configuration interactive unpack_extension).each do |lib|
+   update_project watch_project create_project clean_project extension_command
+   imports installer_command print_version project_stats stamp_pattern
+   sprite validate_project write_configuration interactive unpack_extension
+).each do |lib|
   require "compass/commands/#{lib}"
 end
