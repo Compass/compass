eedaf63d699f64090e96bd6abf2951101cf89808
Stefan Buhrmester <sbuhrmester@cerego.com>
2012-04-20 12:34:00 +0900

    support for multiple image directories

--- lib/compass/configuration/paths.rb
+++ lib/compass/configuration/paths.rb
@@ -5,15 +5,30 @@ module Compass::Configuration::Paths
   def strip_trailing_separator(*attributes)
     attributes.each do |attr|
       alias_method "#{attr}_with_trailing_separator".to_sym, attr
-      class_eval %Q{
-        def #{attr}                                # def css_dir
-          path = #{attr}_with_trailing_separator   #   path = css_dir_with_trailing_separator
-          if path.to_s =~ TRAILING_SEPARATOR       #   if path =~ TRAILING_SEPARATOR
-            path = path[0..-($1.length+1)]         #     path = path[0..-($1.length+1)]
-          end                                      #   end
-          path                                     #   path
-        end                                        # end
-      }
+      if Compass::Configuration::ARRAY_ATTRIBUTES.include? attr
+        class_eval %Q{
+          def #{attr}                                # def css_dir
+            paths = #{attr}_with_trailing_separator  #   paths = css_dir_with_trailing_separator
+            paths.map do |path|
+              if path.to_s =~ TRAILING_SEPARATOR       #   if path =~ TRAILING_SEPARATOR
+                path[0..-($1.length+1)]         #     path = path[0..-($1.length+1)]
+              else
+                path
+              end
+            end                                      #   end                                     #   path
+          end                                        # end
+        }
+      else
+        class_eval %Q{
+          def #{attr}                                # def css_dir
+            path = #{attr}_with_trailing_separator   #   path = css_dir_with_trailing_separator
+            if path.to_s =~ TRAILING_SEPARATOR       #   if path =~ TRAILING_SEPARATOR
+              path = path[0..-($1.length+1)]         #     path = path[0..-($1.length+1)]
+            end                                      #   end
+            path                                     #   path
+          end                                        # end
+        }
+      end
     end
   end
 end
