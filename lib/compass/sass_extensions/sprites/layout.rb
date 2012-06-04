6e7aedd31ca413c25ee346f001cff8fb508c27c0
Scott Davis <jetviper21@gmail.com>
2012-06-04 19:06:43 -0400

    abstracted out vertical layout into an interfaced class

---
+++ lib/compass/sass_extensions/sprites/layout.rb
@@ -0,0 +1,34 @@
+module Compass
+  module SassExtensions
+    module Sprites
+      module Layout
+        class SpriteLayout
+
+          attr_reader :images, :options
+          attr_accessor :height, :width
+
+          def initialize(images, kwargs={})
+            @images  = images
+            @options = kwargs
+            @height  = 0
+            @width   = 0
+            layout!
+            @images
+          end
+
+          def layout!
+            raise Compass::SpriteException, "You must impliment layout!"
+          end
+
+          def properties
+            if [@width, @height].any? { |p| p == 0 }
+              raise Compass::SpriteException, "You must set the width and height before fetching the properties"
+            end
+            [@images, @width, @height]
+          end
+
+        end
+      end
+    end
+  end
+end
