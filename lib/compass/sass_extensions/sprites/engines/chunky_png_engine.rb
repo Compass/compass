e2b8409646c35e2cb47de3c0ab9d68bac5e9ddca
Scott Davis <jetviper21@gmail.com>
2012-06-07 13:37:27 -0400

    added catch in ChunkyPng engine to detect invalid png images closes #860

--- lib/compass/sass_extensions/sprites/engines/chunky_png_engine.rb
+++ lib/compass/sass_extensions/sprites/engines/chunky_png_engine.rb
@@ -12,7 +12,11 @@ module Compass
         def construct_sprite
           @canvas = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
           images.each do |image|
-            input_png  = ChunkyPNG::Image.from_file(image.file)
+            input_png = begin
+              ChunkyPNG::Image.from_file(image.file)
+            rescue ChunkyPNG::SignatureMismatch
+              raise Compass::SpriteException, "You have provided a file that does not have a PNG signature. Only PNG files are supported by the default sprite engine"
+            end
             canvas.replace! input_png, image.left, image.top
           end
         end    
