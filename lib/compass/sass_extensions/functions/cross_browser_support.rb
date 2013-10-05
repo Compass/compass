c72bbfadddbc3d4417fcc32c28b5f6ee65af2376
Chris Eppstein <chris@eppsteins.net>
2013-10-05 07:15:06 -0700

    expose the caniuse api to the stylesheets.

--- lib/compass/sass_extensions/functions/cross_browser_support.rb
+++ lib/compass/sass_extensions/functions/cross_browser_support.rb
@@ -67,4 +67,56 @@ module Compass::SassExtensions::Functions::CrossBrowserSupport
     CSS2FallbackValue.new(value, css2_value)
   end
 
+  def browsers
+    list(Compass::CanIUse.instance.browsers.map{|b| identifier(b)}, :comma)
+  end
+  Sass::Script::Functions.declare(:browsers, [])
+
+  def browser_capabilities
+    list(Compass::CanIUse.instance.capabilities.map{|c| identifier(c)}, :comma)
+  end
+  Sass::Script::Functions.declare(:browser_capabilities, [])
+
+  def browser_versions(browser)
+    assert_type browser, :String
+    list(Compass::CanIUse.instance.versions(browser.value).map{|v| quoted_string(v)}, :comma)
+  rescue ArgumentError => e
+    raise Sass::SyntaxError.new(e.message)
+  end
+  Sass::Script::Functions.declare(:browser_versions, [:browser])
+
+  def browser_requires_prefix(browser, version, capability)
+    assert_type browser, :String
+    assert_type version, :String
+    assert_type capability, :String
+    bool(Compass::CanIUse.instance.requires_prefix(browser.value, version.value, capability.value))
+  rescue ArgumentError => e
+    raise Sass::SyntaxError.new(e.message)
+  end
+  Sass::Script::Functions.declare(:browser_requires_prefix, [:browser, :version, :capability])
+
+  def browser_prefix(browser)
+    assert_type browser, :String
+    identifier(Compass::CanIUse.instance.prefix(browser.value))
+  rescue ArgumentError => e
+    raise Sass::SyntaxError.new(e.message)
+  end
+  Sass::Script::Functions.declare(:browser_prefix, [:browser])
+
+  def browser_prefixes(browser)
+    assert_type browser, :String
+    identifier(Compass::CanIUse.instance.prefix(browser.value))
+  rescue ArgumentError => e
+    raise Sass::SyntaxError.new(e.message)
+  end
+  Sass::Script::Functions.declare(:browser_prefix, [:browser])
+
+  def prefix_usage(prefix, capability)
+    assert_type prefix, :String
+    assert_type capability, :String
+    number(Compass::CanIUse.instance.prefixed_usage(prefix.value, capability.value))
+  rescue ArgumentError => e
+    raise Sass::SyntaxError.new(e.message)
+  end
+  Sass::Script::Functions.declare(:prefix_usage, [:prefix, :capability])
 end
