500ab736919f41dd1a5261e48383522f402f8163
John Riviello <john_riviello@comcast.com>
2013-10-22 12:57:39 -0400

    Moving the search for "compass-*" gems to its own method & calling that method from within discover_extensions!
    Got the command line unit tests passing too.

--- test/units/command_line_test.rb
+++ test/units/command_line_test.rb
@@ -29,6 +29,7 @@ class CommandLineTest < Test::Unit::TestCase
 
   Compass::Frameworks::ALL.each do |framework|
     next if framework.name == "true"
+    next if framework.name == "testing"
     next if framework.name =~ /^_/
     define_method "test_#{framework.name}_installation" do
       within_tmp_directory do
