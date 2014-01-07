616826fb6a1ad62fbacbc89594fdb5cb23084e66
Scott Davis <me@sdavis.info>
2014-01-07 03:19:26 -0500

    move test helpers

---
+++ test/common/helpers.rb
@@ -0,0 +1,7 @@
+helpers_dir =  File.join(File.expand_path('../', __FILE__), 'helpers')
+
+REPO_ROOT = File.expand_path('../../../', __FILE__)
+
+%w(io command_line test_case).each do |helper|
+  require File.join(helpers_dir, helper)
+end
