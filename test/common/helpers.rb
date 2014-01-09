6d5a1fe05ed8b8299108df86064faefbff2618a5
Scott Davis <me@sdavis.info>
2014-01-09 01:26:28 -0500

    added diff

--- test/common/helpers.rb
+++ test/common/helpers.rb
@@ -2,6 +2,6 @@ helpers_dir =  File.join(File.expand_path('../', __FILE__), 'helpers')
 
 REPO_ROOT = File.expand_path('../../../', __FILE__)
 
-%w(io command_line test_case).each do |helper|
+%w(io command_line test_case diff).each do |helper|
   require File.join(helpers_dir, helper)
 end
