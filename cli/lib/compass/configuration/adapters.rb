49c039072ec9ac0073b4d784604e3d7a0050e9dc
Scott Dillender <sodillen@gmail.com>
2014-01-30 09:59:38 -0600

    Clean up watch_load_paths

--- cli/lib/compass/configuration/adapters.rb
+++ cli/lib/compass/configuration/adapters.rb
@@ -70,19 +70,12 @@ module Compass
         Compass::Frameworks::ALL.each do |f|
           load_paths << f.stylesheets_directory if File.directory?(f.stylesheets_directory)
         end
-        importer = sass_options[:filesystem_importer] if sass_options && sass_options[:filesystem_importer]
-        importer ||= Sass::Importers::Filesystem
-        load_paths += resolve_additional_import_paths
-        load_paths.map! do |p|
-          next p if p.respond_to?(:find_relative)
-          importer.new(p.to_s)
-        end
+        load_paths = watch_load_paths(load_paths)
         load_paths << Compass::SpriteImporter.new
         load_paths
       end
 
-      def watch_load_paths
-        load_paths = []
+      def watch_load_paths(load_paths=[])
         importer = sass_options[:filesystem_importer] if sass_options && sass_options[:filesystem_importer]
         importer ||= Sass::Importers::Filesystem
         load_paths += resolve_additional_import_paths
@@ -92,6 +85,7 @@ module Compass
         end
         load_paths
       end
+
     end
     class Data
       include Adapters
