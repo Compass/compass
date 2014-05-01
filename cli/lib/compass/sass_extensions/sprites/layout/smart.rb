cc71016f37cf200a346a1e4293d12935838c2e48
Thierry Michel Philippe Kleist <thierry@codedazur.nl>
2014-05-01 13:36:20 +0200

    various changes

--- cli/lib/compass/sass_extensions/sprites/layout/smart.rb
+++ cli/lib/compass/sass_extensions/sprites/layout/smart.rb
@@ -11,7 +11,7 @@ module Compass
           private # ===========================================================================================>
 
           def calculate_positions!
-            fitter = ::Compass::SassExtensions::Sprites::RowFitter.new(@images, 1900)
+            fitter = ::Compass::SassExtensions::Sprites::RowFitter.new(@images, 1700)
         
             extra_x = 0
             extra_y = 0
