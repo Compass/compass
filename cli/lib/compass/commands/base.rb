e9523d0aec40128120a96d4aa4b5f875b1680c43
Scott Davis <me@sdavis.info>
2014-10-10 22:45:47 -0400

    WIP

--- cli/lib/compass/commands/base.rb
+++ cli/lib/compass/commands/base.rb
@@ -1,8 +1,24 @@
 module Compass
   module Commands
     class Base
+
+      @callbacks = {}
+
       def self.register(command_name)
         Compass::Commands[command_name] = self
+      end
+
+      def self.register_callback(event, &block)
+        @callbacks[event] ||= []
+        @callbacks[event] << block
+      end
+
+      def self.run_callback(event)
+        if @callbacks.has_key?(event)
+          @callbacks[event].each do |callback|
+            callback.call(self)
+          end
+        end
       end
 
       include Actions
@@ -13,7 +29,7 @@ module Compass
         self.working_path = working_path.to_s
         self.options = options
       end
-      
+
       def execute
         perform
       end
